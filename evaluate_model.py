bfaebd0ae48a95cece1e62858e5a7c352df9359a
Drew Thayer <thedrewthayer@gmail.com>
2018-10-22 09:59:21 -0600

    evaluation script working

--- evaluate_model.py
+++ evaluate_model.py
@@ -6,6 +6,7 @@ from keras.layers import Flatten
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
 from keras.models import model_from_json
+from sklearn.metrics import mean_squared_error
 
 from DataTools.pickle import save_to_pickle, load_from_pickle
 
@@ -19,14 +20,51 @@ if __name__=='__main__':
     model.load_weights('models/model_1.h5')
     print('model and weights loaded')
 
-    # load data
+    # load data: train set in inputs/outputs, test set
     X_train = load_from_pickle('data_Xy', 'X_train.pkl')
     y_train = load_from_pickle('data_Xy', 'y_train.pkl')
+    test_df = load_from_pickle('data', 'data_test.pkl')
+    test = test_df.values
 
-    # compile
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    # compile and forecast
+    def forecast(model, X_train, n_input):
+        # grab the last week in training data
+        input_x = X_train[-n_input:,0]
+        # reshape into [1, n_input, 1]
+        input_x = input_x.reshape((1, len(input_x), 1))
+        # predict the next week
+        model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+        y_hat = model.predict(input_x, verbose=1)
+        return y_hat
 
-    # evaluate
-    score = model.evaluate(X_train, y_train, verbose=1)
-    print('\n{}: {:0.2f}'.format(model.metrics_names[0], score[0]))
-    print('{}: {:0.2f}%'.format(model.metrics_names[1], score[1]*100))
+    y_hat = forecast(model, X_train, n_input=7)
+
+    # evaluate on test set: first feature
+    test = test[:,0]
+    # walk-forward validation for each week
+    n_input = 7
+    true = []
+    pred = []
+    c = 0
+    for i in range(len(test) - (2*n_input)):
+        input = test[c:c + n_input]
+        input = input.reshape(1, len(input), 1)
+        y_true = test[c + n_input: c + 2*n_input]
+        y_hat = model.predict(input, verbose=1)
+        true.append(y_true)
+        pred.append(y_hat.squeeze())
+        print(c)
+        c += 1
+
+    # score predictions
+    errors = []
+    for y_true, y_hat in zip(true, pred):
+        mse = mean_squared_error(y_true, y_hat)
+        rmse = np.sqrt(mse)
+        errors.append(rmse)
+
+
+
+    #score = model.evaluate(X_train, y_train, verbose=1)
+    #print('\n{}: {:0.2f}'.format(model.metrics_names[0], score[0]))
+    #print('{}: {:0.2f}%'.format(model.metrics_names[1], score[1]*100))
